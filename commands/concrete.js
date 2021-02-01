d87a88e0454639b6f4a2e004a2db68937b869e70
William Ford <williamthefatman@gmail.com>
2021-02-01 00:36:40 -0500

    x

---
+++ commands/concrete.js
@@ -0,0 +1,38 @@
+module.exports = {
+    name: "laugh",
+    description: "Plays a laugh track",
+    async execute(message, args) {
+      const { channel } = message.member.voice;
+      const queue = message.client.queue.get(message.guild.id);
+  
+      const queueConstruct = {
+        textChannel: message.channel,
+        channel,
+        connection: null,
+        songs: [],
+        loop: false,
+        volume: 100,
+        playing: true
+      };
+  
+      message.client.queue.set(message.guild.id, queueConstruct);
+  
+      try {
+        queueConstruct.connection = await channel.join();
+        const dispatcher = queueConstruct.connection
+          .play('./sounds/concrete.mp3')
+          .on('finish', () => {
+            message.client.queue.delete(message.guild.id);
+            channel.leave();
+          })
+          .on("error", err => {
+            message.client.queue.delete(message.guild.id);
+            channel.leave();
+            console.error(err);
+          });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+  
