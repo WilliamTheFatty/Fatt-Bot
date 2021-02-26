ce8a9d0c9957a0d492d5ece6775d5d3a554da71c
WilliamTheFatMan <williamthefatman@gmail.com>
2021-02-26 00:37:46 -0500

    penis

--- commands/penis.js
+++ commands/penis.js
@@ -1,6 +1,6 @@
 module.exports = {
-    name: "concrete",
-    description: "FUCK YEAH CONCRETE",
+    name: "penis",
+    description: "tyler googles penis",
     async execute(message, args) {
       const { channel } = message.member.voice;
       const queue = message.client.queue.get(message.guild.id);
